c691ffaa7820ca2f3d46a9b3950bd445b233800f
Baek Gi Chan <laphayen@gmail.com>
2024-08-24 22:54:15 +0900

    #7 - Add PassEntity for managing pass information
    - Implemented PassEntity class with fields: passSeq, packageSeq, userId, status, remainingCount, startedAt, endedAt, and expiredAt.
    - Set up primary key with auto-increment (GenerationType.IDENTITY) for passSeq.
    - Added PassStatus field for tracking the status of the pass.
    - Included timestamps for start, end, and expiration dates.
    - Extended BaseEntity for common fields and auditing.

---
+++ src/main/java/com/laphayen/ptpassbatch/repository/pass/PassEntity.java
@@ -0,0 +1,30 @@
+package com.laphayen.ptpassbatch.repository.pass;
+
+import com.laphayen.ptpassbatch.repository.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "pass")
+public class PassEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer passSeq;
+    private Integer packageSeq;
+    private String userId;
+
+    private PassStatus status;
+    private Integer remainingCount;
+
+    private LocalDateTime startedAt;
+    private LocalDateTime endedAt;
+    private LocalDateTime expiredAt;
+
+}
