4333979fe423ea85fbaa6fb048e974bd5c93a747
Baek Gi Chan <laphayen@gmail.com>
2024-08-28 00:01:05 +0900

    #7 - Add BookingRepository interface for BookingEntity management
    - Implemented BookingRepository extending JpaRepository for CRUD operations on BookingEntity.
    - Configured the repository with BookingEntity and Integer as the ID type.

---
+++ src/main/java/com/laphayen/ptpassbatch/repository/booking/BookingRepository.java
@@ -0,0 +1,7 @@
+package com.laphayen.ptpassbatch.repository.booking;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<BookingEntity, Integer> {
+
+}
